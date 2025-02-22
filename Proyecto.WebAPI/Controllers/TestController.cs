06b06ba11540d54f3a9c68fdbc9e3d717a717242
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2025-02-22 21:36:50 +0100

    Initital Commit

---
+++ Proyecto.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    [Route("test")]  // Makes the endpoint available at /api/test/test
+    public IActionResult GetTestMessage()
+    {
+        return Ok(new { message = "API is working" });
+    }
+}
