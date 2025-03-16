caabf260ce2c75d85d0e0314159d9ed672f812b1
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2025-03-16 17:45:15 +0100

    Fase Inicio: ediciones pequeñas en el código

--- Proyecto.Web/obj/Debug/net6.0/Proyecto.Web.AssemblyInfo.cs
+++ Proyecto.Web/obj/Debug/net6.0/Proyecto.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bbb7dcaeb91d4a1ab244c58c457331d1a340d9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bce06a6b0e25fe4195bf13ea74711bdb2209bd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
