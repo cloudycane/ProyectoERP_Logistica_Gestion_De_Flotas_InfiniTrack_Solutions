3ecf184b80a68d85a065119b673993830ba5184a
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2025-03-24 22:01:14 +0100

    Fase Inicial: Creación de Entidades

---
+++ Proyecto.Core/DTOs/CategoriaActivosDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Core.DTOs
+{
+    public class CategoriaActivosDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [StringLength(25, ErrorMessage = "Este campo requiere un mínimo de entre 1 y {0} carácteres.")]
+        [Display(Name = "Nombre de la Categoría")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [StringLength(250, ErrorMessage = "Este campo requiere un mínimo de entre 1 y {0} carácteres.")]
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Proyecto.Core/Entidades/CategoriaActivosModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Proyecto.Core.Entidades
+{
+    public class CategoriaActivosModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripción { get; set; }
+    }
+}
